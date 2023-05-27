740f0d622a1832cb48fc6644c1ddfad172695c11
GoodComrade <goodcomrade2106@gmail.com>
2023-05-28 02:01:17 +0300

    Add props on the scene

--- Assets/Scripts/Utils/PropFadingDetector.cs
+++ Assets/Scripts/Utils/PropFadingDetector.cs
@@ -1,26 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Events;
 
 [RequireComponent(typeof(CameraFollow))]
 public class PropFadingDetector : MonoBehaviour
 {
-    public event UnityAction<string> OnPropBetween;
-
     [SerializeField]
-    private List<PropTransparenter> _props;
+    private List<GameObject> _propsObjects;
+
+    private List<PropTransparenter> _propsTransparenters = new List<PropTransparenter>();
 
     private Transform _target;
     private RaycastHit _hit;
 
+    private void Awake()
+    {
+        
+    }
     void Start()
     {
         _target = GetComponent<CameraFollow>().Target;
-        foreach (var prop in _props)
-        {
-            OnPropBetween += prop.SetDetectedProp;
-        }
+        foreach (var prop in _propsObjects)
+            _propsTransparenters.Add(prop.GetComponent<PropTransparenter>());
     }
 
     //попробуй переделать это на события, где если имя объекта не совпадает, то и прозрачность не выставляется
@@ -31,7 +34,18 @@ public class PropFadingDetector : MonoBehaviour
 
         if (Physics.Raycast(transform.position, rayDirection, out _hit))
         {
-            OnPropBetween.Invoke(_hit.collider.gameObject.name);
+            Debug.DrawRay(_target.position, transform.position, Color.red);
+
+            if (_hit.collider.gameObject.name != _target.gameObject.name)
+            {
+                var detectedProp = _propsTransparenters.First(prop => prop.gameObject.name == _hit.collider.gameObject.name);
+                detectedProp.SetDetectedProp(_hit.collider.gameObject.name);
+            }
+            else
+            {
+                _propsTransparenters.ForEach(prop => prop.SetDetectedProp(_target.gameObject.name));
+            }
+            //OnPropBetween.Invoke(_hit.collider.gameObject.name);
         }
     }
 }
